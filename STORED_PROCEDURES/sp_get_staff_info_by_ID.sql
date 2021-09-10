527ffbb0bf2cab3762da7f05b27849cfb41e4764
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-10 23:04:43 +0300

    Update sp_get_staff_info_by_ID.sql

--- STORED_PROCEDURES/sp_get_staff_info_by_ID.sql
+++ STORED_PROCEDURES/sp_get_staff_info_by_ID.sql
@@ -2,12 +2,12 @@ IF OBJECT_ID('Sp_GetStaffInfoByID','P') IS NOT NULL
     DROP PROCEDURE Sp_GetStaffInfoByID
 GO
 
-CREATE PROCEDURE Sp_GetStaffInfoByID @ID int
+CREATE PROCEDURE Sp_GetStaffInfoByID @ID INT
 AS
 BEGIN
-	SELECT (First_Name+' '+Last_Name) AS [Full Name],
-		    Phone,
-		    Email 
+	SELECT First_Name+' '+Last_Name) AS [Full Name],
+	       Phone,
+	       Email 
 	FROM tbStaffs
 	WHERE ID = @ID
 END
