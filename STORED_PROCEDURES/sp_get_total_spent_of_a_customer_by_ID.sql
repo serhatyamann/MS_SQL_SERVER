0b4304ea3bb491fb63d0da2f3b3a03f33ee687a4
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-10 23:45:53 +0300

    Create sp_get_total_spent_of_a_customer_by_ID.sql

---
+++ STORED_PROCEDURES/sp_get_total_spent_of_a_customer_by_ID.sql
@@ -0,0 +1,16 @@
+IF OBJECT_ID('Sp_GetTotalSpentOfACustomerByID','P') IS NOT NULL
+    DROP PROCEDURE Sp_GetTotalSpentOfACustomerByID
+GO
+
+CREATE PROCEDURE Sp_GetTotalSpentOfACustomerByID @ID INT
+AS
+BEGIN
+	SELECT c.First_Name +SPACE(1)+ c.Last_Name as [Full Name],
+		   oi.OrderID,
+		   SUM(quantity * list_price * (1-discount)) as [Total Spent]
+	FROM tbCustomers c
+	JOIN tbOrders o ON c.CustomerID = o.CustomerID
+	JOIN tbOrderItems oi on o.OrderID = oi.OrderID
+	WHERE c.customer_id = @ID
+	GROUP BY oi.OrderID, c.First_Name +SPACE(1)+ c.Last_Name
+END
