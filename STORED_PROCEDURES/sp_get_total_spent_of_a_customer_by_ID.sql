88a782556829eff9fc1351a17696b2118886675d
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-10 23:52:11 +0300

    Update sp_get_total_spent_of_a_customer_by_ID.sql

--- STORED_PROCEDURES/sp_get_total_spent_of_a_customer_by_ID.sql
+++ STORED_PROCEDURES/sp_get_total_spent_of_a_customer_by_ID.sql
@@ -7,11 +7,11 @@ AS
 BEGIN
 	SELECT c.First_Name +SPACE(1)+ c.Last_Name as [Full Name],
 		   oi.OrderID,
-		   SUM(quantity * list_price * (1-discount)) as [Total Spent]
+		   SUM(Quantity * ListPrice * (1-Discount)) as [Total Spent]
 	FROM tbCustomers c
 	JOIN tbOrders o ON c.CustomerID = o.CustomerID
 	JOIN tbOrderItems oi on o.OrderID = oi.OrderID
-	WHERE c.customer_id = @ID
+	WHERE c.CustomerID = @ID
 	GROUP BY oi.OrderID, c.First_Name +SPACE(1)+ c.Last_Name
 	ORDER BY oi.OrderID ASC
 END
