cd5070c53daf3b10d5ab1a5c38936425e6644633
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-12 04:52:06 +0300

    Update view_daily_sales.sql

--- VIEWS/view_daily_sales.sql
+++ VIEWS/view_daily_sales.sql
@@ -5,8 +5,8 @@ GO
 CREATE VIEW view_Daily_Sales
 AS
 SELECT order_date,
-	   product_name,
-	   quantity * oi.list_price * (1-discount) as [Total Sale]
+       product_name,
+       quantity * oi.list_price * (1-discount) as [Total Sale]
 FROM order_items oi
 JOIN products p on p.product_id = oi.product_id
 JOIN orders o on o.order_id = oi.order_id
