c1f54cdf1b7a004a59075b5c8c96752a09ade5d8
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-12 04:51:22 +0300

    Create view_daily_sales.sql

---
+++ VIEWS/view_daily_sales.sql
@@ -0,0 +1,17 @@
+IF OBJECT_ID('view_Daily_Sales','V') IS NOT NULL
+	DROP VIEW view_Daily_Sales
+GO
+
+CREATE VIEW view_Daily_Sales
+AS
+SELECT order_date,
+	   product_name,
+	   quantity * oi.list_price * (1-discount) as [Total Sale]
+FROM order_items oi
+JOIN products p on p.product_id = oi.product_id
+JOIN orders o on o.order_id = oi.order_id
+GO
+
+--TEST
+
+SELECT * FROM view_Daily_Sales
