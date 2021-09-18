af0aae1341f44b4686868ac143bd995d724a7359
Serhat Yaman <serhatyaman@hotmail.com.tr>
2021-09-18 05:20:11 +0300

    Create use_of_tran_in_sp.sql

---
+++ TRANSACTIONS/use_of_tran_in_sp.sql
@@ -0,0 +1,78 @@
+IF OBJECT_ID('Sp_InsertOrder','P') IS NOT NULL
+    DROP PROCEDURE Sp_InsertOrder
+GO
+
+CREATE PROCEDURE Sp_InsertOrder (
+	@customer_id INT,
+	@order_status TINYINT,
+	@order_date DATE,
+	@required_date DATE,
+	@shipped_date DATE,
+	@store_id INT,
+	@staff_id INT,
+	@product_id INT,
+	@quantity INT,
+	@list_price DECIMAL(10,2),
+	@discount DECIMAL(4,2)
+
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+	INSERT INTO orders 
+				(customer_id,
+				 order_status,
+				 order_date,
+			     required_date,
+				 shipped_date,
+				 store_id,
+				 staff_id)
+	VALUES
+				(@customer_id,
+				 @order_status,
+			     @order_date,
+			     @required_date,
+			     @shipped_date,
+				 @store_id,
+				 @staff_id)
+				 
+	DECLARE @orderID INT
+	SELECT @orderID = @@IDENTITY
+
+	INSERT INTO order_items 
+				(order_id,
+				 product_id,
+				 quantity,
+				 list_price,
+				 discount)
+	VALUES
+				(@orderID,
+				 @product_id,
+				 @quantity,
+				 @list_price,
+				 @discount)
+
+	UPDATE stocks
+	SET
+				 quantity = quantity - @quantity
+	WHERE
+				 store_id = @store_id AND
+				 product_id = @product_id
+				 
+		COMMIT TRANSACTION
+	END TRY
+	BEGIN CATCH 
+		PRINT 'ERROR WHILE PROCESSING ORDER'
+	--  If needed, you can see the error message with the code below
+	--	DECLARE @msg NVARCHAR(MAX)
+	--	set @msg = (SELECT ERROR_MESSAGE())
+	--	print @msg;
+		ROLLBACK TRANSACTION
+	END CATCH	
+END
+GO
+
+--TEST
+
+EXEC Sp_InsertOrder 1212,4,'2016/01/01','2016/01/06','2016/01/09',1,7,247,7,300,0.05;
